c0456adb73770f5ec84115fdc3253004191f446d
Farah <aiwithfarah@gmail.com>
2025-12-31 18:41:14 +0530

    Add files via upload

---
+++ Dart Fundamentals/funcation_example.dart
@@ -0,0 +1,3 @@
+// Function is a named block of code that performs a specific task.
+// Instead of writing the same code multiple times, you define it once and "call" it whenever you need it.
+// This is essential for the DRY Principle (Don't Repeat Yourself)
---
+++ Dart Fundamentals/variables_example.dart
@@ -0,0 +1,15 @@
+// In the world of Flutter, the lib folder (short for "Library") is where all your actual code lives.
+
+// UNDERSTANDING VARIABLES
+
+void main() {
+  String brand_name = 'Spice Meve';
+  int stock_count = 1000;
+  double pricePerJar = 399.00;
+  bool is_spicy = true;
+
+  print('Brand Name: $brand_name');
+  print('Stock Count: $stock_count');
+  print('Price Per Jar: $pricePerJar');
+  print('Is it Spicy?: $is_spicy');
+}
