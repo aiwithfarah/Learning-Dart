3d068b09e2ae0c682262b87b6605e11277fa8e7a
Farah <aiwithfarah@gmail.com>
2026-01-04 17:27:12 +0530

    Add files via upload

---
+++ Enums/enums_example.dart
@@ -0,0 +1,22 @@
+// This is a "Structure" for logic. Sometimes you have a variable that can only be one of a few specific options.
+// Example: Connection Status. It can only be Connected, Disconnected, or Connecting.
+// Bad Way: Using Strings. status = "Conected" (Spelling mistake breaks the code!).
+// Good Way: Using an Enum (Enumeration). It forces you to pick from a valid list.
+
+//Define enums outside main
+
+enum ConnectionStatus { connected, disconnected, connecting }
+
+void main() {
+  //Use it
+
+  var status = ConnectionStatus.connecting;
+
+  if (status == ConnectionStatus.connected) {
+    print('Online');
+  } else {
+    print('Please Wait!');
+  }
+}
+
+//Please Wait!
