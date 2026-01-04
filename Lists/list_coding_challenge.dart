da93bd4a377c9478d49d0e5178482b6315385303
Farah <aiwithfarah@gmail.com>
2026-01-04 15:34:52 +0530

    Add files via upload

---
+++ Lists/list_coding_challenge.dart
@@ -0,0 +1,12 @@
+//------------Challenge-----------------
+// Create a List<String> named menu inside main().
+// Add 3 food items to it (e.g., "Biryani", "Curry", "Naan").
+// Print the first item.
+// Print the last item (the 3rd one).
+
+void main() {
+  List<String> foods = ['Biryani', 'Curry', 'Naan'];
+
+  print(foods[0]);
+  print(foods[2]);
+}
---
+++ Lists/lists_example.dart
@@ -0,0 +1,8 @@
+// List<String> tells Dart: "This is a list that ONLY holds Strings"
+
+void main() {
+  List<String> mySpices = ['cinnamon', 'clove', 'cardamom'];
+
+  //The first item is at index 0.
+  print(mySpices[0]);
+}
