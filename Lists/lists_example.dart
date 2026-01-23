f0429a77a72a7638edae8ac27970458661a38700
Farah <aiwithfarah@gmail.com>
2026-01-23 20:20:01 +0530

    Update lists_example.dart

--- Lists/lists_example.dart
+++ Lists/lists_example.dart
@@ -1,13 +1,25 @@
+
+List (The Ordered Row)
+
+What it is: Ordered group of items. Duplicates allowed.
+
+Use for: Feeds, Todo lists, Repository lists.
+
+Syntax: ['A', 'B', 'A']
+
+Access: By Index (list[0]).
+  
 If you make a List final, you can still change the items inside it (add/remove), you just can't replace the whole list.
 
 If you make a List const, the whole thing is frozen. You cannot add, remove, or change anything inside.
 
 // List<String> tells Dart: "This is a list that ONLY holds Strings"
-
+----------------------------------------------------------------------------------------------------------------------------
 void main() {
   List<String> mySpices = ['cinnamon', 'clove', 'cardamom'];
 
   //The first item is at index 0.
   print(mySpices[0]);
 }
+
 
